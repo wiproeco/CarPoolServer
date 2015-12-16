1fdc1afcf2afa17f62180c4eb0dcb3c890b7bfad
= <=>
2015-12-16 14:49:27 +0530

    initial commit

---
+++ CarPoolExpress/appsocket.js
@@ -0,0 +1,33 @@
+﻿
+var server = require('http').createServer();
+var io = require('socket.io')(server);
+var users = {};
+io.sockets.on('connection', function (socket) {
+    console.log('socket server is listening on port 3000');
+	
+var socketUserid='';
+socket.on('regUser', function (result) {
+	socketUserid = result.userid;
+users[socketUserid] = socket;
+   console.log('new user ' + socketUserid+ ' registered');
+
+});
+
+var notificationmsg = '';
+var userid ='';
+socket.on('sendNotification', function (request) {
+	notificationmsg = request.data;
+	userid = request.userid;
+    //console.log(request.data);
+    if(users[userid] != 'undefined'){
+            var receiverSocket = users[userid];
+          receiverSocket.emit('recieveNotification', notificationmsg);
+    }
+ });
+
+    
+});
+
+exports.CreateSocketPort = function() {
+  server.listen(3000);
+};
--- CarPoolExpress/server.js
+++ CarPoolExpress/server.js
@@ -5,5 +5,11 @@ var app = require('./app');
 app.set('port', process.env.PORT || 1513);
 
 var server = app.listen(app.get('port'), function() {
+    console.log('Express server connection connected on port 1513');
     debug('Express server listening on port ' + server.address().port);
 });
+
+
+var appsocket = require('./appsocket');
+var x=appsocket.CreateSocketPort();
+
